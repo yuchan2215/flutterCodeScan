57edebbfe5af2e7c146a029f1fc09e52653f7cdc
Miyayu <yuchan2215@gmail.com>
2022-05-26 19:58:54 +0900

    [add] 基本的なQRコードリーダーを作成

--- lib/widgets/cameraPage.dart
+++ lib/widgets/cameraPage.dart
@@ -1,19 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'dart:io';
+import 'dart:developer';
+import 'dart:math';
 
 import '../app.dart';
 
 class CameraPage extends StatefulWidget {
-  const CameraPage({Key? key}): super(key:key);
+  const CameraPage({Key? key}) : super(key: key);
+
   @override
   _CameraPageState createState() => _CameraPageState();
 }
-class _CameraPageState extends State<CameraPage>{
+
+class _CameraPageState extends State<CameraPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  QRViewController? controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller?.pauseCamera();
+    }
+    controller?.resumeCamera();
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      print("Hello");
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text("読み込み"),
       ),
+      body: Stack(
+        children: [
+          _buildQrView(context),
+        ],
+      ),
     );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    var scanArea = min(width, height) * 0.9;
+
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+        borderColor: Theme.of(context).primaryColor,
+        borderRadius: 10,
+        borderLength: 30,
+        borderWidth: 10,
+        cutOutSize: scanArea,
+      ),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) async {
+      print(scanData.code);
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    print('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('no Permission')),
+      );
+    }
   }
 }
