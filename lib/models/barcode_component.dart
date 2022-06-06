1601e2e980e9fc4bbe607438386f6e5975dec8ae
Miyayu <yuchan2215@gmail.com>
2022-06-06 09:44:17 +0900

    [add] タップと長押しをした時の処理を作成

--- lib/models/barcode_component.dart
+++ lib/models/barcode_component.dart
@@ -1,8 +1,38 @@
+import 'package:flutter/material.dart';
+
 enum BarcodeComponentType {
   tel,
   email,
   url,
   other,
+}
+
+extension BarcodeComponentTypeExt on BarcodeComponentType {
+  Icon? get getIcon {
+    switch (this) {
+      case BarcodeComponentType.email:
+        return const Icon(Icons.mail);
+      case BarcodeComponentType.tel:
+        return const Icon(Icons.phone);
+      case BarcodeComponentType.url:
+        return const Icon(Icons.link);
+      default:
+        return null;
+    }
+  }
+  String? get actionName {
+    switch (this) {
+      case BarcodeComponentType.email:
+        return "メール";
+      case BarcodeComponentType.tel:
+        return "電話番号";
+      case BarcodeComponentType.url:
+        return "URL";
+      default:
+        return null;
+    }
+
+  }
 }
 
 class BarcodeComponent {
--- lib/pages/result.dart
+++ lib/pages/result.dart
@@ -1,5 +1,10 @@
 import 'package:codereader/extensions/barcode.dart';
+import 'package:codereader/models/barcode_component.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ResultPageState extends StatefulWidget {
   const ResultPageState({Key? key}) : super(key: key);
@@ -54,35 +59,45 @@ class _ResultPageStateState extends State<ResultPageState> {
     return [
       ...item.components.where((e) => e.content != null).map(
         (e) {
-          if (e.isMemo) { //メモの時はアイコンのみにする
+          if (e.isMemo) {
+            //メモの時はアイコンのみにする
             return ListTile(
               leading: const Icon(Icons.info),
               title: Text(e.content!),
             );
-          } else { //メモでないときはアイコンにする。
+          } else {
+            //メモでないときはアイコンにする。
             return Card(
               margin: const EdgeInsets.all(16),
-              child: Container(
-                margin: const EdgeInsets.only(
-                    left: 5, top: 5, bottom: 16, right: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "${e.title}:",
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium
-                          ?.copyWith(color: Colors.grey),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 16.0, top: 8.0),
-                      child: Text(
-                        e.content!,
-                        style: Theme.of(context).textTheme.bodyText1,
+              child: InkWell(
+                onLongPress: () {
+                  tapEvent(e: e);
+                },
+                onTap: () {
+                  tapEvent(e: e);
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(
+                      left: 5, top: 5, bottom: 16, right: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${e.title}:",
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.copyWith(color: Colors.grey),
                       ),
-                    ),
-                  ],
+                      Container(
+                        margin: const EdgeInsets.only(left: 16.0, top: 8.0),
+                        child: Text(
+                          e.content!,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
@@ -90,6 +105,87 @@ class _ResultPageStateState extends State<ResultPageState> {
         },
       ),
     ];
+  }
+
+  void tapEvent({required BarcodeComponent e}) {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (context) => CupertinoActionSheet(
+        title: Text(e.content!),
+        actions: [
+          //コピーボタン
+          CupertinoActionSheetAction(
+            onPressed: () {
+              Clipboard.setData(
+                ClipboardData(text: e.content!),
+              );
+              Navigator.of(context).pop();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("クリップボードにコピーしました。")),
+              );
+            },
+            child: const Text("コピー"),
+          ),
+          //共有ボタン
+          CupertinoActionSheetAction(
+            onPressed: () {
+              Navigator.of(context).pop();
+              Share.share("${e.content!} ");
+            },
+            child: const Text("共有"),
+          ),
+          //開くボタン
+          ...getButtons(e),
+        ],
+        cancelButton: CupertinoActionSheetAction(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text("キャンセル"),
+        ),
+      ),
+    );
+  }
+
+  List<CupertinoActionSheetAction> getButtons(BarcodeComponent component) {
+    switch (component.type) {
+      case BarcodeComponentType.tel:
+        return [
+          CupertinoActionSheetAction(
+            onPressed: () {
+              launchUrl(Uri.parse("tel:${component.content!}"));
+            },
+            child: const Text("電話"),
+          ),
+          CupertinoActionSheetAction(
+            onPressed: () {
+              launchUrl(Uri.parse("sms:${component.content!}"));
+            },
+            child: const Text("SMS"),
+          )
+        ];
+      case BarcodeComponentType.email:
+        return [
+          CupertinoActionSheetAction(
+            onPressed: () {
+              launchUrl(Uri.parse("mailto:${component.content!}"));
+            },
+            child: const Text("開く"),
+          )
+        ];
+      case BarcodeComponentType.url:
+        return [
+          CupertinoActionSheetAction(
+            onPressed: () {
+              launchUrl(Uri.parse(component.content!),
+                  mode: LaunchMode.externalApplication);
+            },
+            child: const Text("開く"),
+          )
+        ];
+      default:
+        return [];
+    }
   }
 
   List<Widget> advancedItems() {
